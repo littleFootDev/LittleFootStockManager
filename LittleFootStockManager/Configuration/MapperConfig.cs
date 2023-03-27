2ba5343562a3248a95c9070fc4da90ffbe8a123c
littleFootDev <thelittlefootdev@gmail.com>
2023-03-27 19:18:35 +0200

    Adding map for User and userdto

---
+++ LittleFootStockManager/Configuration/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LittleFootStockManager.Data.Model;
+using LittleFootStockManager.Dto.User;
+using System.Diagnostics.Metrics;
+
+namespace LittleFootStockManager.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            //User
+            CreateMap<Users, UserDto>().ReverseMap();
+        }
+    }
+}
