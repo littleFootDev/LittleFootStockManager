c178ebd395009ce018e675ee4e67eea96cf11de2
littleFootDev <thelittlefootdev@gmail.com>
2023-03-27 12:02:49 +0200

    Adding configuration for cors and log

--- LittleFootStockManager/Program.cs
+++ LittleFootStockManager/Program.cs
@@ -1,9 +1,25 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b =>
+                b.AllowAnyHeader()
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+    );
+});
+
+builder.Host.UseSerilog((ctx, LoggerConfiguration) =>
+{
+    LoggerConfiguration.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration);
+});
 
 var app = builder.Build();
 
@@ -15,6 +31,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.Run();
 
