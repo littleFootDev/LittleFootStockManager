76c6ac965dbf88cb85fea7b7a4145ae1f1d6da4f
littleFootDev <thelittlefootdev@gmail.com>
2023-03-27 19:05:00 +0200

    Adding dto for users

---
+++ LittleFootStockManager/Dto/User/AuthReponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LittleFootStockManager.Dto.User
+{
+    public class AuthReponseDto
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
