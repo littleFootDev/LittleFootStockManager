ba413166800f29a408d1a7a5211144374948b56a
littleFootDev <thelittlefootdev@gmail.com>
2023-03-29 11:11:49 +0200

    Adding configuration migration

--- LittleFootStockManager/Data/LittleFootStockManagerDbContext.cs
+++ LittleFootStockManager/Data/LittleFootStockManagerDbContext.cs
@@ -17,7 +17,67 @@ namespace LittleFootStockManager.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasMany(u => u.Products)
+                    .WithOne(p => p.User)
+                    .HasForeignKey(p => p.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(u => u.Events)
+                    .WithOne(p => p.User)
+                    .HasForeignKey(p => p.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Name).IsRequired().HasMaxLength(128);
+                entity.Property(p => p.Description).IsRequired().HasMaxLength(1024);
+                entity.Property(p => p.Category).IsRequired().HasMaxLength(50);
+                entity.Property(p => p.Quantity).IsRequired();
+                entity.Property(p => p.QuantityAvaible).IsRequired();
+                entity.Property(p => p.Price).HasColumnType("decimal(18,2)").IsRequired();
+                entity.Property(p => p.ImagePath).IsRequired();
+
+
+                entity.HasOne(p => p.User)
+                    .WithMany(u => u.Products)
+                    .HasForeignKey(p => p.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(o => o.Id);
+                entity.Property(o => o.NameClient).IsRequired().HasMaxLength(50);
+                entity.Property(o => o.CreatedAt).IsRequired();
+                entity.Property(o => o.DeliveryDate).IsRequired();
+                entity.Property(o => o.IsPayed).IsRequired();
+                entity.Property(o => o.AmountTotal).HasColumnType("decimal(18,2)").IsRequired();
+                entity.Property(o => o.IsArchive).IsRequired();
+
+                entity.HasMany<Product>(o => o.Products)
+                    .WithOne();
+            });
+
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.NameClient).IsRequired().HasMaxLength(128);
+                entity.Property(p => p.Lieux).IsRequired().HasMaxLength(128);
+                entity.Property(p => p.Address).IsRequired().HasMaxLength(128);
+                entity.Property(p => p.City).IsRequired().HasMaxLength(128);
+                entity.Property(p => p.DDayDate).IsRequired();
+
+                entity.HasMany<Product>(o => o.Products)
+                    .WithOne();
+            });
+
         }
     }
 
--- LittleFootStockManager/Data/Migrations/20230329090821_ConfigurationMigration.Designer.cs
+++ LittleFootStockManager/Data/Migrations/20230329090821_ConfigurationMigration.Designer.cs
@@ -4,6 +4,7 @@ using LittleFootStockManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LittleFootStockManager.Data.Migrations
 {
     [DbContext(typeof(LittleFootStockManagerDbContext))]
-    partial class LittleFootStockManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230329090821_ConfigurationMigration")]
+    partial class ConfigurationMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
