37d4fdc00e90f07b3d976110c211377c33de3e7c
littleFootDev <thelittlefootdev@gmail.com>
2023-04-01 09:45:34 +0200

    Adding gestion of user

--- LittleFootStockManager/Contract/IAdminRepository.cs
+++ LittleFootStockManager/Contract/IAdminRepository.cs
@@ -1,4 +1,5 @@
-﻿using LittleFootStockManager.Dto.Admin;
+﻿using LittleFootStockManager.Data.Model;
+using LittleFootStockManager.Dto.Admin;
 using Microsoft.AspNetCore.Identity;
 
 namespace LittleFootStockManager.Contract
@@ -11,5 +12,16 @@ namespace LittleFootStockManager.Contract
         public Task<bool> UpdateRole(RoleInfoDto updateRole);
         public Task<bool> DeleteRole(string roleId);
         public Task<bool> ConfirmeDeleteRole(IdentityRole role);
+
+        //User
+        public Task<AdminUserIndexDto> GetAllUsers();
+        public Task<bool> DeleteUser(string userId);
+        public Task<bool> ConfirmeDeleteUser(Users user);
+        public Task<bool> RemoveRoleUser(string userId, string roleName);
+        public Task<bool> AddRoleUser(string userId, string roleName);
+        public Task<bool> AddClaimUser(string userId, string claimType, string claimValue);
+        public Task<bool> RemoveClaimUser(string userId, string claimType);
+        public Task<bool> GetUser(string userId);
+        public Task<UpdateUserDto> UpdateUser(Users updateUserDto);
     }
 }
--- LittleFootStockManager/Repository/AdminRepository.cs
+++ LittleFootStockManager/Repository/AdminRepository.cs
@@ -1,8 +1,11 @@
 ﻿using LittleFootStockManager.Contract;
 using LittleFootStockManager.Data;
+using LittleFootStockManager.Data.Model;
 using LittleFootStockManager.Dto.Admin;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Crypto.Tls;
+using System.Security.Claims;
 
 namespace LittleFootStockManager.Repository
 {
@@ -10,11 +13,13 @@ namespace LittleFootStockManager.Repository
     {
         private readonly LittleFootStockManagerDbContext _context;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<Users> _userManager;
 
-        public AdminRepository(LittleFootStockManagerDbContext context, RoleManager<IdentityRole> roleManager)
+        public AdminRepository(LittleFootStockManagerDbContext context, RoleManager<IdentityRole> roleManager, UserManager<Users> userManager)
         {
             this._context = context;
             this._roleManager = roleManager;
+            this._userManager = userManager;
         }
         #region role
         public async Task<bool> ConfirmeDeleteRole(IdentityRole role)
@@ -39,7 +44,6 @@ namespace LittleFootStockManager.Repository
             }
 
         }
-
         public async Task<bool> CreateRole(RoleDto roleDto)
         {
             try
@@ -61,7 +65,6 @@ namespace LittleFootStockManager.Repository
                 throw new Exception($"An error occurred while creating the role.{ex.Message}");
             }
         }
-
         public async Task<bool> DeleteRole(string roleId)
         {
             try
@@ -82,7 +85,6 @@ namespace LittleFootStockManager.Repository
                 throw new Exception($"An error occurred while deleting the role.{ex.Message}");
             }
         }
-
         public async Task<AdminRolesIndexDto> GetAllRoles()
         {
             try
@@ -98,7 +100,6 @@ namespace LittleFootStockManager.Repository
                 throw new Exception($"An error occurred while retrieving the roles. {ex.Message}");
             }
         }
-
         public async Task<bool> UpdateRole(RoleInfoDto updateRole)
         {
             try
@@ -121,6 +122,183 @@ namespace LittleFootStockManager.Repository
             {
                 throw new Exception($"An error occurred while updating the role.{ex.Message}");
             }
+        }
+
+        #endregion
+        #region User
+        public async Task<bool> AddClaimUser(string userId, string claimType, string claimValue)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(userId);
+                if (user == null)
+                    throw new Exception($"User with id {userId} not found");
+
+                var claim = new Claim(claimType, claimValue);
+                var result = await _userManager.AddClaimAsync(user, claim);
+                if (!result.Succeeded)
+                    throw new Exception($"Error adding claim {claimType}:{claimValue} to user with id {userId}");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error adding claim {claimType}:{claimValue} to user with id {userId}", ex);
+            }
+        }
+
+        public async Task<bool> AddRoleUser(string userId, string roleName)
+        {   try
+            {
+                var user = await _context.Users.FindAsync(userId);
+                if (user == null)
+                    throw new Exception($"User with id {userId} not found");
+
+                var role = await _context.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
+                if (role == null)
+                    throw new Exception($"Role with name {roleName} not found");
+
+                var result = await _userManager.AddToRoleAsync(user, roleName);
+                if (!result.Succeeded)
+                    throw new Exception($"Error adding role {roleName} to user with id {userId}");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error adding role {roleName} to user with id {userId} : {ex.Message} ");
+            }
+        }
+        public async Task<UpdateUserDto> UpdateUser(Users updateUserDto)
+        {
+            try
+            {
+                var newUser = new UpdateUserDto
+                {
+                    Id = updateUserDto.Id,
+                    FirstName = updateUserDto.FirstName,
+                    LastName = updateUserDto.LastName,
+                };
+                newUser.Roles = await _context.Roles.Select(r => r.Name).ToArrayAsync();
+                newUser.UserRoles = await _userManager.GetRolesAsync(updateUserDto);
+                newUser.UserClaims = await _userManager.GetClaimsAsync(updateUserDto);
+
+                return newUser;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error updating user with id {updateUserDto.Id}", ex);
+            }
+        }
+        public async Task<AdminUserIndexDto> GetAllUsers()
+        {
+            try
+            {
+                var users = await _context.Users.AsNoTracking().ToArrayAsync();
+                return new AdminUserIndexDto
+                {
+                    Users = users,
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error getting all users : {ex.Message}");
+            }
+        }
+
+        public async Task<bool> GetUser(string userId)
+        {
+            try
+            {
+                var user = await _userManager.FindByIdAsync(userId);
+
+                if (user == null)
+                    throw new Exception($"User with id {userId} not found");
+
+                var result = await UpdateUser(user);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error getting user with id {userId} : {ex.Message}");
+            }
+        }
+
+        public async Task<bool> RemoveClaimUser(string userId, string claimType)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(userId);
+                if (user is null)
+                    throw new Exception($"User with id {userId} not found");
+
+                var claims = await _userManager.GetClaimsAsync(user);
+                if (claims == null)
+                    throw new Exception($"Claim with type {claimType} not found for user with id {userId}");
+
+                if(claims.Count() > 0)
+                {
+                    var claim = claims.FirstOrDefault(c => c.Type == claimType);
+                    var result = await _userManager.RemoveClaimAsync(user, claim);
+                    if (!result.Succeeded)
+                        throw new Exception($"Error removing claim {claimType} from user with id {userId}");
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error removing claim {claimType} from user with id {userId}", ex);
+            }
+        }
+
+        public async Task<bool> RemoveRoleUser(string userId, string roleName)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(userId);
+                if (user == null)
+                    throw new Exception($"User with id {userId} not found");
+
+                var role = await _context.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
+                if (role == null)
+                    throw new Exception($"Role with name {roleName} not found");
+
+                var result = await _userManager.RemoveFromRoleAsync(user, role.Name);
+                if (!result.Succeeded)
+                    throw new Exception($"Error removing role {roleName} from user with id {userId}");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error removing role {roleName} from user with id {userId} {ex.Message}");
+            }
+        }
+        public async Task<bool> DeleteUser(string userId)
+        {
+            try
+            {
+                var user = await _context.Users.FindAsync(userId);
+                if (user == null)
+                    throw new Exception($"User with id {userId} not found");
+
+                var result = await ConfirmeDeleteUser(user);
+                if(result == true)
+                {
+                    return true;
+                }
+                throw new Exception($"Error deleting user with id {userId}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error deleting user with id {userId}: {ex.Message}");
+            }
+        }
+        public async Task<bool> ConfirmeDeleteUser(Users user)
+        {
+            await _userManager.DeleteAsync(user);
+            return true;
         }
         #endregion
     }
